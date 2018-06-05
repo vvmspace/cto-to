0d020d956823caa9e1bb7cdb81b8f730246b08af
vvmsecured <vvmspace@gmail.com>
2018-06-05 12:47:21 +0500

    TheSimplest

--- js/background.js
+++ js/background.js
@@ -3,7 +3,28 @@ const CB = new CrossB();
 class BG {
 
     constructor(){
+        this.initTicker();
+    }
 
+    tick(){
+        $.getJSON('http://tbot.vvm.space/api/sin/cto-btc', (data) => {
+            // alert(JSON.stringify(data.to));
+            chrome.browserAction.setBadgeText({
+                text: data.to.toString()
+            });
+            chrome.browserAction.setBadgeBackgroundColor({
+                color: "green"
+            });
+        });
+    }
+
+    initTicker(){
+        setInterval(
+            () => {
+                this.tick();
+            },
+            1000
+        )
     }
 
     static init(){
